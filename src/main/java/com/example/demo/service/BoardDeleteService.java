1ca532f5e639adb8cc67bf267907c2d0de1579bd
qjarms568 <qjarms568@naver.com>
2023-08-22 21:56:58 +0900

    글삭제 진행중.

--- src/main/java/com/example/demo/service/BoardDeleteService.java
+++ src/main/java/com/example/demo/service/BoardDeleteService.java
@@ -3,7 +3,6 @@ package com.example.demo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.DTO.BoardSearchAllDTO;
 import com.example.demo.mapper.BoardMapper;
 
 @Service
@@ -12,4 +11,8 @@ public class BoardDeleteService {
     @Autowired
     private BoardMapper boardMapper;
 
+
+    public void delete(String id) {
+        boardMapper.beleteBoard(id);
+    }
 }
